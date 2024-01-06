fd62bd1f08f8ca2d7afe6908a57ff18ba8c3d737
bricked <git@bricked.dev>
2024-01-06 23:07:39 +0100

    configure cava

--- modules/home/cava/default.nix
+++ modules/home/cava/default.nix
@@ -1,9 +1,32 @@
 { config, pkgs, lib, ... }:
 
-with lib; {
-  options.modules.cava = {enable = mkEnableOption "cava"; };
+with lib;
 
-  config = mkIf config.modules.cava.enable {
-    home.packages = with pkgs; [ kitty ];
+let cfg = config.modules.cava;
+in {
+  options.modules.cava = {
+    enable = mkEnableOption "Enable Cava";
+  };
+
+  config = mkIf cfg.enable {
+    programs.cava = {
+      enable = true;
+
+      settings = {
+        input.method = "pipewire";
+        color = {
+          gradient = "1";
+          gradient_count = "8";
+          gradient_color_1 = "'#b4befe'";
+          gradient_color_2 = "'#89b4fa'";
+          gradient_color_3 = "'#89dceb'";
+          gradient_color_4 = "'#a6e3a1'";
+          gradient_color_5 = "'#f9e2af'";
+          gradient_color_6 = "'#fab387'";
+          gradient_color_7 = "'#eba0ac'";
+          gradient_color_8 = "'#f38ba8'";
+        };
+      };
+    };
   };
 }
