6f3653bf869b03d556b4726e7f027e0cae04ed31
bricked <git@bricked.dev>
2024-01-07 17:40:18 +0100

    enable webgl on librewolf

--- modules/home/librewolf/default.nix
+++ modules/home/librewolf/default.nix
@@ -8,6 +8,9 @@ with lib; {
   config = mkIf config.modules.librewolf.enable {
     programs.librewolf = {
       enable = true;
-    }
+      settings = {
+        "webgl.disabled" = false;
+      };
+    };
   };
 }
