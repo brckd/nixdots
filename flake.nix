d67db804ce803eaaeade2c3e61a3eb68c748d345
bricked <git@bricked.dev>
2024-02-29 20:04:46 +0000

    upgrade nixpkgs and home to unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
