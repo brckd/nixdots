b5683d364a3bcf57cf8baa418b0909ec9431b49b
bricked <git@bricked.dev>
2024-03-20 19:56:45 +0100

    hyprland: format config

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -5,53 +5,34 @@
 }:
 with lib; let
   cfg = config.wayland.windowManager.hyprland;
+  mainMod = "SUPER";
+  binds = {
+    C = "kitty";
+    F = "librewolf";
+    D = "vesktop";
+  };
 in {
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland = {
       settings = {
         monitor = ",preferred,auto,auto";
 
-        env = "XCURSOR_SIZE,24";
-
-        input = {
-          follow_mouse = "1";
-
-          touchpad = {
-            natural_scroll = "no";
-          };
-
-          sensitivity = "0";
-        };
-
         general = {
-          gaps_in = "5";
-          gaps_out = "10";
-          border_size = "0";
-
           layout = "dwindle";
-
-          allow_tearing = "false";
+          gaps_in = 5;
+          gaps_out = 10;
+          border_size = 0;
         };
 
         decoration = {
-          rounding = "10";
-
-          drop_shadow = "true";
-          shadow_range = "4";
-          shadow_render_power = "3";
-
-          active_opacity = "1";
-          inactive_opacity = "1";
-
-          dim_inactive = "true";
-          dim_strength = "0.2";
+          rounding = 10;
+          dim_inactive = true;
+          dim_strength = 0.2;
         };
 
         animations = {
-          enabled = "yes";
-
+          enabled = true;
           bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
           animation = [
             "windows, 1, 7, myBezier"
             "windowsOut, 1, 7, default, popin 80%"
@@ -63,78 +44,44 @@ in {
         };
 
         dwindle = {
-          pseudotile = "yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below";
-          preserve_split = "yes # you probably want this";
+          pseudotile = true;
+          preserve_split = true;
         };
 
-        master = {
-          new_is_master = "true";
-        };
+        master.new_is_master = true;
 
-        gestures = {
-          workspace_swipe = "off";
-        };
+        bind =
+          [
+            "${mainMod} SHIFT, Q, exit, "
+            "${mainMod}, Q, killactive, "
+            "${mainMod}, V, togglefloating, "
+            "${mainMod}, P, pseudo," # dwindle
+            "${mainMod}, J, togglesplit," # dwindle
 
-        misc = {
-          force_default_wallpaper = "-1 # Set to 0 to disable the anime mascot wallpapers";
-        };
+            # Move focus with mainMod + arrow keys
+            "${mainMod}, left, movefocus, l"
+            "${mainMod}, right, movefocus, r"
+            "${mainMod}, up, movefocus, u"
+            "${mainMod}, down, movefocus, d"
 
-        "$mainMod" = "SUPER";
+            # Example special workspace (scratchpad)
+            "${mainMod}, S, togglespecialworkspace, magic"
+            "${mainMod} SHIFT, S, movetoworkspace, special:magic"
 
-        bind = [
-          "$mainMod SHIFT, C, exec, kitty"
-          "$mainMod SHIFT, F, exec, librewolf"
-          "$mainMod, Q, killactive, "
-          "$mainMod SHIFT, Q, exit, "
-          "$mainMod SHIFT, E, exec, dolphin"
-          "$mainMod, V, togglefloating, "
-          "$mainMod, R, exec, wofi --show drun"
-          "$mainMod, P, pseudo, # dwindle"
-          "$mainMod, J, togglesplit, # dwindle"
-
-          # Move focus with mainMod + arrow keys
-          "$mainMod, left, movefocus, l"
-          "$mainMod, right, movefocus, r"
-          "$mainMod, up, movefocus, u"
-          "$mainMod, down, movefocus, d"
-
+            # Scroll through existing workspaces with mainMod + scroll
+            "${mainMod}, mouse_down, workspace, e+1"
+            "${mainMod}, mouse_up, workspace, e-1"
+          ]
+          ++ mapAttrsToList (key: app: "${mainMod} SHIFT, ${key}, exec, ${app}") binds
           # Switch workspaces with mainMod + [0-9]
-          "$mainMod, 1, workspace, 1"
-          "$mainMod, 2, workspace, 2"
-          "$mainMod, 3, workspace, 3"
-          "$mainMod, 4, workspace, 4"
-          "$mainMod, 5, workspace, 5"
-          "$mainMod, 6, workspace, 6"
-          "$mainMod, 7, workspace, 7"
-          "$mainMod, 8, workspace, 8"
-          "$mainMod, 9, workspace, 9"
-          "$mainMod, 0, workspace, 10"
-
+          ++ map (ws: "${mainMod}, ${toString (mod ws 10)}, workspace, ${toString ws}") (range 1 10)
           # Move active window to a workspace with mainMod + SHIFT + [0-9]
-          "$mainMod SHIFT, 1, movetoworkspace, 1"
-          "$mainMod SHIFT, 2, movetoworkspace, 2"
-          "$mainMod SHIFT, 3, movetoworkspace, 3"
-          "$mainMod SHIFT, 4, movetoworkspace, 4"
-          "$mainMod SHIFT, 5, movetoworkspace, 5"
-          "$mainMod SHIFT, 6, movetoworkspace, 6"
-          "$mainMod SHIFT, 7, movetoworkspace, 7"
-          "$mainMod SHIFT, 8, movetoworkspace, 8"
-          "$mainMod SHIFT, 9, movetoworkspace, 9"
-          "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-          # Example special workspace (scratchpad)
-          "$mainMod, S, togglespecialworkspace, magic"
-          "$mainMod SHIFT, S, movetoworkspace, special:magic"
-
-          # Scroll through existing workspaces with mainMod + scroll
-          "$mainMod, mouse_down, workspace, e+1"
-          "$mainMod, mouse_up, workspace, e-1"
-        ];
+          ++ map (ws: "${mainMod} SHIFT, ${toString (mod ws 10)}, movetoworkspace, ${toString ws}") (range 1 10);
 
         # Move/resize windows with mainMod + LMB/RMB and dragging
         bindm = [
-          "$mainMod, mouse:272, movewindow"
-          "$mainMod, mouse:273, resizewindow"
+          "${mainMod}, mouse:272, movewindow"
+          "${mainMod}, mouse:273, resizewindow"
         ];
       };
     };
