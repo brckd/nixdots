9db5e3817605677f497033c6fd04f6ac93fb33af
bricked <git@bricked.dev>
2024-03-26 13:19:01 +0100

    global: nix format

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -80,7 +80,7 @@ in {
               {name = "luasnip";}
             ];
             mapping = {
-							"<C-Space>" = "cmp.mapping.complete()";
+              "<C-Space>" = "cmp.mapping.complete()";
               "<CR>" = "cmp.mapping.confirm({ select = true })";
               "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
               "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
