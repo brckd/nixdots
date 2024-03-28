cf4ab5943761bb72f1c07c337efe4597b00b923e
bricked <git@bricked.dev>
2024-03-28 16:04:29 +0100

    ags: add stylix colors

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -5,10 +5,18 @@
 }:
 with lib; let
   cfg = config.programs.ags;
+  colors = config.lib.stylix.colors;
+	colorNames = (map (n: "base${fixedWidthNumber 2 n}") (range 0 16));
 in {
   config = mkIf cfg.enable {
     programs.ags = {
       configDir = ./.;
     };
+
+		xdg.configFile."ags".recursive = true;
+    xdg.configFile."ags/src/style/colors.css".text =
+      concatMapStringsSep "\n"
+      (color: "@define-color ${color} ${colors.withHashtag.${color}};")
+			colorNames;
   };
 }
