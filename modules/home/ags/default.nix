a06eb80f5b03bb0b680ea26df0932caf0825f052
bricked <git@bricked.dev>
2024-03-28 17:45:15 +0100

    ags: add simple bar

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+	pkgs,
   ...
 }:
 with lib; let
@@ -12,11 +13,16 @@ in {
     programs.ags = {
       configDir = ./.;
     };
-
 		xdg.configFile."ags".recursive = true;
+
     xdg.configFile."ags/src/style/colors.css".text =
       concatMapStringsSep "\n"
       (color: "@define-color ${color} ${colors.withHashtag.${color}};")
 			colorNames;
+
+		xdg.configFile."ags/src/assets/nixos-symbolic.svg".source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/35ebbbf01c3119005ed180726c388a01d4d1100c/logo/white.svg";
+        hash = "sha256-Ed2l6i2wi/YTcWCq23bspH/t3RYp6AodykpXF1Zgccw=";
+		};
   };
 }
