7b872534605a3ae5812895d3e319f66b438a7f6d
bricked <git@bricked.dev>
2024-03-29 22:45:23 +0100

    fmt: add treefmt to use multiple formatters

--- modules/home/ags/default.nix
+++ modules/home/ags/default.nix
@@ -1,28 +1,28 @@
 {
   config,
   lib,
-	pkgs,
+  pkgs,
   ...
 }:
 with lib; let
   cfg = config.programs.ags;
   colors = config.lib.stylix.colors;
-	colorNames = (map (n: "base${fixedWidthNumber 2 n}") (range 0 16));
+  colorNames = map (n: "base${fixedWidthNumber 2 n}") (range 0 16);
 in {
   config = mkIf cfg.enable {
     programs.ags = {
       configDir = ./.;
     };
-		xdg.configFile."ags".recursive = true;
+    xdg.configFile."ags".recursive = true;
 
     xdg.configFile."ags/src/style/colors.css".text =
       concatMapStringsSep "\n"
       (color: "@define-color ${color} ${colors.withHashtag.${color}};")
-			colorNames;
+      colorNames;
 
-		xdg.configFile."ags/src/assets/nixos-symbolic.svg".source = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/35ebbbf01c3119005ed180726c388a01d4d1100c/logo/white.svg";
-        hash = "sha256-Ed2l6i2wi/YTcWCq23bspH/t3RYp6AodykpXF1Zgccw=";
-		};
+    xdg.configFile."ags/src/assets/nixos-symbolic.svg".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/35ebbbf01c3119005ed180726c388a01d4d1100c/logo/white.svg";
+      hash = "sha256-Ed2l6i2wi/YTcWCq23bspH/t3RYp6AodykpXF1Zgccw=";
+    };
   };
 }
