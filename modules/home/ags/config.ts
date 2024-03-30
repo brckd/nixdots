deb7e65f5fe45cfc6c2b13a69e87793566ae9150
bricked <git@bricked.dev>
2024-03-30 23:16:00 +0100

    ags: move main stylesheet

--- modules/home/ags/config.ts
+++ modules/home/ags/config.ts
@@ -1,6 +1,6 @@
 import Bar from "./widget/Bar";
 // @ts-ignore
-import style from "./style.scss";
+import style from "./style/main.scss";
 
 App.addIcons(`${App.configDir}/assets`);
 App.applyCss(style);
