5d53342e3df325b43ce8154b3cb34e58499c39d6
bricked <git@bricked.dev>
2024-07-05 17:23:39 +0200

    ags: add logout button

--- modules/home/ags/widget/PowerMenu.ts
+++ modules/home/ags/widget/PowerMenu.ts
@@ -1,14 +1,24 @@
-export const PowerButton = (command: string, icon: string) =>
+const hyprland = await Service.import("hyprland");
+
+export const LogOutButton = () =>
   Widget.Button({
     className: "power button",
-    child: Widget.Icon(icon),
-    onClicked: () => Utils.execAsync(command),
+    child: Widget.Icon("system-log-out-symbolic"),
+    onClicked: () => hyprland.messageAsync("dispatch exit"),
+  });
+
+export const ShutdownButton = () =>
+  Widget.Button({
+    className: "power button",
+    child: Widget.Icon("system-shutdown-symbolic"),
+    onClicked: () => Utils.execAsync("shutdown now"),
   });
 
 export const PowerMenu = () =>
   Widget.Box({
     className: "power menu-bar",
-    children: [PowerButton("shutdown now", "system-shutdown-symbolic")],
+    spacing: 5,
+    children: [LogOutButton(), ShutdownButton()],
   });
 
 export default PowerMenu;
