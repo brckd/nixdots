782f0edf5b9fcc7adf1b2cc2b59b4ba14893188a
bricked <git@bricked.dev>
2024-07-06 17:47:53 +0200

    ags: scale volume slider

--- modules/home/ags/widget/Volume.ts
+++ modules/home/ags/widget/Volume.ts
@@ -5,14 +5,15 @@ export enum Type {
   Microphone = "microphone",
 }
 
-export const VolumeSlider = (type = Type.Speaker) =>
+export const VolumeSlider = (type = Type.Speaker, scale = 5) =>
   Widget.Slider({
     className: "progress",
     hexpand: true,
     drawValue: false,
+    max: 1 / scale,
 
-    onChange: ({ value }) => (audio[type].volume = value),
-    value: audio[type].bind("volume"),
+    onChange: ({ value }) => (audio[type].volume = value * scale),
+    value: audio[type].bind("volume").as((v) => v / scale),
   });
 
 export function getIcon(volume: number, isMuted?: boolean | null) {
@@ -21,18 +22,17 @@ export function getIcon(volume: number, isMuted?: boolean | null) {
   return `audio-volume-${icons[i]}-symbolic`;
 }
 
-export const VolumeIcon = (type = Type.Speaker) => 
-	Widget.Button({
-		className: "button",
-		child:
-		Widget.Icon({
-			icon: Utils.merge(
-				[audio[type].bind("volume"), audio[type].bind("is_muted")],
-				getIcon,
-			),
-		}),
-		onClicked: () => audio[type].is_muted = !audio[type].is_muted
-	})
+export const VolumeIcon = (type = Type.Speaker) =>
+  Widget.Button({
+    className: "button",
+    child: Widget.Icon({
+      icon: Utils.merge(
+        [audio[type].bind("volume"), audio[type].bind("is_muted")],
+        getIcon,
+      ),
+    }),
+    onClicked: () => (audio[type].is_muted = !audio[type].is_muted),
+  });
 
 export const Volume = (type = Type.Speaker) =>
   Widget.Box({
