b6a97e8e92ffd5628a870889febd8db8c59efef6
bricked <git@bricked.dev>
2024-07-10 22:35:25 +0200

    lf: don't enable ctpv automatically
    its nixpkg is currently broken because of nose dependency

--- modules/home/lf/default.nix
+++ modules/home/lf/default.nix
@@ -7,16 +7,20 @@
 with lib; let
   cfg = config.programs.lf;
 in {
+  options.programs.lf = {
+    ctpv.enable = mkEnableOption "ctpv plugin";
+  };
+
   config = mkIf cfg.enable {
     programs.lf = {
       commands = {
         q = "quit";
       };
-      previewer = {
+      previewer = mkIf cfg.ctpv.enable {
         keybinding = "i";
         source = "${pkgs.ctpv}/bin/ctpv";
       };
-      extraConfig = ''
+      extraConfig = mkIf cfg.ctpv.enable ''
         &${pkgs.ctpv}/bin/ctpv -s $id
         cmd on-quit %${pkgs.ctpv}/bin/ctpv -e $id
         set cleaner ${pkgs.ctpv}/bin/ctpvclear
