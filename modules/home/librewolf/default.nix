44337842e5b3b501d0f5976e1127c290a5b97646
bricked <git@bricked.dev>
2024-09-24 18:57:07 +0200

    librewolf: add generic firefox settings

--- modules/home/librewolf/default.nix
+++ modules/home/librewolf/default.nix
@@ -11,6 +11,48 @@ in {
       settings = {
         "webgl.disabled" = false;
       };
+      profiles = {
+        default = {
+          name = "Default";
+          extensions = with config.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            bitwarden
+            darkreader
+            libredirect
+          ];
+          search = {
+            default = "DuckDuckGo";
+            force = true;
+            engines = {
+              "Google".metaData.hidden = true;
+              "Bing".metaData.hidden = true;
+            };
+          };
+          bookmarks = [
+            {
+              name = "GitHub";
+              tags = ["git"];
+              keyword = "github";
+              url = "https://github.com/";
+            }
+          ];
+          settings = {
+            "extensions.autoDisableScopes" = 0; # Enable extensions
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # Enable userchrome
+            "browser.aboutConfig.showWarning" = false;
+
+            # Blank homepage
+            "browser.newtabpage.enable" = false;
+            "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
+            "browser.toolbars.bookmarks.visibility" = "never";
+
+            # Toolbar customization
+            "browser.uiCustomization.state" = builtins.readFile ./toolbar.json;
+          };
+
+          userChrome = builtins.readFile ./userChrome.css;
+        };
+      };
     };
   };
 }
