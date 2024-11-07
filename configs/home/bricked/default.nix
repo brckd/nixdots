fa41e8c37b83b0d08aeac0b5bf939a8320a77eea
bricked <git@bricked.dev>
2024-11-07 21:29:43 +0100

    home: configure gpg for git

--- configs/home/bricked/default.nix
+++ configs/home/bricked/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   home = {
     username = "bricked";
     homeDirectory = "/home/bricked";
@@ -20,7 +20,14 @@
     extraConfig = {
       push.autoSetupRemote = true;
       pull.rebase = true;
+      commit.gpgsign = true;
     };
+  };
+  services.gnome-keyring.enable = true;
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
   programs.gh.enable = true;
   programs.npm.enable = true;
