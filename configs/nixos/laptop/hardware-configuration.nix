7ef789bcaad8e74c36dcd54d70214216311de6b5
bricked <git@bricked.dev>
2024-11-16 13:38:04 +0100

    laptop: refactor partitions and use lvm

--- configs/nixos/laptop/hardware-configuration.nix
+++ configs/nixos/laptop/hardware-configuration.nix
@@ -12,24 +12,31 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "sd_mod" "sdhci_acpi" "rtsx_usb_sdmmc"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "uas" "sd_mod" "sdhci_acpi" "rtsx_usb_sdmmc"];
+  boot.initrd.kernelModules = ["dm-snapshot"];
+  boot.kernelModules = ["kvm-intel" "cryptd"];
+  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-label/LUKS";
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/821922b7-7e01-440e-b005-9880ac00e2f1";
+    device = "/dev/disk/by-label/NIXOS";
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."luks-2875592c-ce00-48d9-8b48-bd0016e122d4".device = "/dev/disk/by-uuid/2875592c-ce00-48d9-8b48-bd0016e122d4";
-
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/EA05-0FC2";
+    device = "/dev/disk/by-label/BOOT";
     fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [];
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/HOME";
+    fsType = "ext4";
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-label/SWAP";}
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -39,5 +46,6 @@
   # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
