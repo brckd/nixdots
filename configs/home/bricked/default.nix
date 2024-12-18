773ecff10d28f131639d5aa08c53f93491b1fd72
bricked <git@bricked.dev>
2024-12-18 19:19:54 +0100

    bricked: add cavalier

--- configs/home/bricked/default.nix
+++ configs/home/bricked/default.nix
@@ -42,5 +42,29 @@
   # Apps
   programs.librewolf.enable = true;
   programs.spicetify.enable = true;
+  programs.cavalier = {
+    enable = true;
+    settings.general = {
+      ShowControls = true;
+      ColorProfiles = [
+        {
+          Name = "Default";
+          FgColors = [
+            "#ffed333b"
+            "#ffffa348"
+            "#fff8e45c"
+            "#ff57e389"
+            "#ff62a0ea"
+            "#ffc061cb"
+          ];
+          BgColors = [
+            "#ff1e1e2e"
+          ];
+          Theme = 1;
+        }
+      ];
+      ActiveProfile = 0;
+    };
+  };
   home.packages = with pkgs; [vesktop];
 }
