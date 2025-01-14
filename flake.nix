db13f44bdd1627b7aa7bb053f3edcad50d2ced1a
bricked <git@bricked.dev>
2025-01-14 22:31:11 +0100

    flake: pin nix-on-droid input to 24.05 release

--- flake.nix
+++ flake.nix
@@ -130,7 +130,7 @@
     };
 
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid";
+      url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-for-bootstrap.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
