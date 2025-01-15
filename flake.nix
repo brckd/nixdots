08b3f7da183f11239e06e9d6ca0f3184de27d873
bricked <git@bricked.dev>
2025-01-15 11:09:04 +0100

    flake: add nix-on-droid binary cache

--- flake.nix
+++ flake.nix
@@ -98,12 +98,14 @@
 
   nixConfig = {
     substituters = [
-      "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://nix-on-droid.cachix.org"
     ];
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
     ];
   };
 
