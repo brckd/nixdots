274b998ae16d0c6e9a9f5c41d75ac6306b4302f8
bricked <git@bricked.dev>
2025-01-20 18:26:28 +0100

    kitty: add keybinding to copy ansi

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -14,6 +14,10 @@ in {
         single_window_padding_width = 10;
         linux_display_server = "x11"; # Fixes header bar on gnome
       };
+
+      keybindings = {
+        "ctrl+shift+alt+c" = "copy_ansi_to_clipboard";
+      };
     };
   };
 }
