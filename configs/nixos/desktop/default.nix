aa5dd8982edafa92c28f09d36f39eef8cefd5fe1
bricked <git@bricked.dev>
2025-01-28 14:28:04 +0100

    desktop: use base16 for moar

--- configs/nixos/desktop/default.nix
+++ configs/nixos/desktop/default.nix
@@ -203,7 +203,7 @@ with lib; {
   environment.variables = {
     EDITOR = "nvim";
     PAGER = "moar";
-    MOAR = "--wrap --quit-if-one-screen --colors 8";
+    MOAR = "--wrap --quit-if-one-screen --colors 16";
   };
 
   services.flatpak.packages = [
