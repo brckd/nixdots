41a236bf7487232c30c3cc68e90c6b95bfb34347
bricked <git@bricked.dev>
2025-03-18 17:23:04 +0100

    flake: use own nixpkgs fork

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
 
   inputs = {
     # Package reposities
-    nixpkgs.url = "github:nixos/nixpkgs/a7abebc31a8f60011277437e000eebcc01702b9f";
+    nixpkgs.url = "github:brckd/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nur = {
       url = "github:nix-community/NUR";
