c028f66eb2bebc09d31c16532edf10d103c0fffa
bricked <git@bricked.dev>
2025-06-10 16:40:36 +0200

    modules/librewolf: update search to use icon attr

--- modules/librewolf/home.nix
+++ modules/librewolf/home.nix
@@ -33,27 +33,27 @@ in {
               "Bing".metaData.hidden = true;
               "Startpage" = {
                 urls = singleton {template = "https://www.startpage.com/sp/search?query={searchTerms}";};
-                iconsUpdateURL = "https://www.startpage.com/sp/cdn/favicons/favicon-96x96.png";
+                icon = "https://www.startpage.com/sp/cdn/favicons/favicon-96x96.png";
                 definedAliases = ["@sp" "@startpage"];
               };
               "Noogle" = {
                 urls = singleton {template = "https://noogle.dev/q?term={searchTerms}";};
-                iconsUpdateURL = "https://noogle.dev/favicon.png";
+                icon = "https://noogle.dev/favicon.png";
                 definedAliases = ["@ng" "@noogle"];
               };
               "Nix Documentation" = {
                 urls = singleton {template = "https://nix.dev/search.html?q={searchTerms}";};
-                iconsUpdateURL = "https://nix.dev/_static/favicon.png";
+                icon = "https://nix.dev/_static/favicon.png";
                 definedAliases = ["@nxd" "@nixdocs"];
               };
               "Nix Manual" = {
                 urls = singleton {template = "https://nix.dev/manual/nix/latest/?search={searchTerms}";};
-                iconUpdateURL = "https://nix.dev/manual/nix/latest/favicon.svg";
+                icon = "https://nix.dev/manual/nix/latest/favicon.svg";
                 definedAliases = ["@nxm" "@nixmanual"];
               };
               "Nix Packages" = {
                 urls = singleton {template = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";};
-                iconUpdateURL = "https://search.nixos.org/favicon.png";
+                icon = "https://search.nixos.org/favicon.png";
                 definedAliases = ["@nxp" "@nixpackages"];
               };
               "Searchix" = {
@@ -62,37 +62,37 @@ in {
               };
               "NixOS Options" = {
                 urls = singleton {template = "https://search.nixos.org/options?query={searchTerms}";};
-                iconUpdateURL = "https://search.nixos.org/favicon.png";
+                icon = "https://search.nixos.org/favicon.png";
                 definedAliases = ["@noo" "@nixosops"];
               };
               "NixOS Wiki" = {
                 urls = singleton {template = "https://nixos.wiki/index.php?search={searchTerms}";};
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                icon = "https://nixos.wiki/favicon.png";
                 definedAliases = ["@now" "@nixoswiki"];
               };
               "Home Manager Options" = {
                 urls = singleton {template = "https://home-manager-options.extranix.com/?query={searchTerms}";};
-                iconUpdateURL = "https://home-manager-options.extranix.com/images/favicon.png";
+                icon = "https://home-manager-options.extranix.com/images/favicon.png";
                 definedAliases = ["@hmo" "@homemmanageropts"];
               };
               "Flake Parts Docs" = {
                 urls = singleton {template = "https://flake.parts/?search={searchTerms}";};
-                iconUpdateURL = "https://flake.parts/favicon.svg";
+                icon = "https://flake.parts/favicon.svg";
                 definedAliases = ["@fpd" "flakepartsdocs"];
               };
               "NixVim Docs" = {
                 urls = singleton {template = "https://nix-community.github.io/nixvim/?search={searchTerms}";};
-                iconUpdateURL = "https://nix-community.github.io/nixvim/favicon.svg";
+                icon = "https://nix-community.github.io/nixvim/favicon.svg";
                 definedAliases = ["@nvd" "@nixvimdocs"];
               };
               "Fancade Wiki" = {
                 urls = singleton {template = "https://www.fancade.com/wiki/gollum/search?q={searchTerms}";};
-                iconUpdateURL = "https://www.fancade.com/favicon.ico";
+                icon = "https://www.fancade.com/favicon.ico";
                 definedAliases = ["@fcw" "@fancadewiki"];
               };
               "Deepl" = {
                 urls = singleton {template = "https://www.deepl.com/en/translator#en/en/{searchTerms}";};
-                iconUpdateURL = "https://static.deepl.com/img/logo/deepl-logo-blue.svg";
+                icon = "https://static.deepl.com/img/logo/deepl-logo-blue.svg";
                 definedAliases = ["@dpl" "@deepl"];
               };
             };
