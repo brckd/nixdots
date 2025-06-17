788707691895b51901632a0e55f5c85e1c19ea35
bricked <git@bricked.dev>
2025-06-17 17:15:45 +0200

    modules/helix: configure editor

--- modules/helix/home.nix
+++ modules/helix/home.nix
@@ -8,58 +8,110 @@ with lib; let
   cfg = config.programs.helix;
 in {
   config = mkIf cfg.enable {
-    programs.helix.languages = {
-      language-server = {
-        astro-ls = {
-          command = "astro-ls";
-          args = ["--stdio"];
-          config = {
-            typescript.tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib";
-            environment = "node";
-          };
-        };
-
-        nixd = {
-          command = "nixd";
-          formatting.command = ["alejandra"];
-        };
-
-        rust-analyzer = {
-          config.check.command = "clippy";
-        };
-
-        tinymist = {
-          command = "tinymist";
-        };
-
-        yaml-language-server = {
-          config.yaml.schemas = {
-            "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml, yaml}";
-          };
+    programs.helix = {
+      settings = {
+        editor = {
+          auto-format = false;
+          cursor-shape.insert = "bar";
+          file-picker.hidden = false;
+          whitespace.render.tab = "all";
         };
       };
 
-      language = [
-        {
-          name = "astro";
-          language-servers = ["astro-ls"];
-        }
-        {
-          name = "nix";
-          language-servers = ["nixd"];
-        }
-        {
-          name = "toml";
-          formatter = {
-            command = "taplo";
-            args = ["fmt" "-"];
+      languages = {
+        language-server = {
+          astro-ls = {
+            command = "astro-ls";
+            args = ["--stdio"];
+            config = {
+              typescript.tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib";
+              environment = "node";
+            };
           };
-        }
-        {
-          name = "typst";
-          language-servers = ["tinymist"];
-        }
-      ];
+
+          vscode-json-language-server = {
+            config.json.schemas = [
+              {
+                fileMatch = ["tsconfig.json"];
+                url = "https://json.schemastore.org/tsconfig";
+              }
+            ];
+          };
+
+          nixd = {
+            command = "nixd";
+          };
+
+          rust-analyzer = {
+            config.check.command = "clippy";
+          };
+
+          tinymist = {
+            command = "tinymist";
+          };
+
+          yaml-language-server = {
+            config.yaml.schemas = {
+              "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml, yaml}";
+            };
+          };
+        };
+
+        language =
+          [
+            {
+              name = "astro";
+              language-servers = ["astro-ls"];
+            }
+            {
+              name = "nix";
+              language-servers = ["nixd"];
+              formatter.command = "alejandra";
+            }
+            {
+              name = "toml";
+              formatter = {
+                command = "taplo";
+                args = ["fmt" "-"];
+              };
+            }
+            {
+              name = "typst";
+              language-servers = ["tinymist"];
+              formatter.command = "typstyle";
+            }
+          ]
+          ++ map (
+            {
+              name,
+              extension ? name,
+            }: {
+              inherit name;
+              formatter = {
+                command = "biome";
+                args = ["format" "--stdin-file-path" "stdin.${extension}" "--indent-style" "space"];
+              };
+            }
+          ) [
+            {
+              name = "javascript";
+              extension = "js";
+            }
+            {
+              name = "typescript";
+              extension = "ts";
+            }
+            {
+              name = "jsx";
+            }
+            {
+              name = "tsx";
+            }
+            {
+              name = "json";
+            }
+          ];
+      };
     };
 
     home.packages = with pkgs; [
@@ -67,13 +119,16 @@ in {
       vscode-langservers-extracted # HTML, CSS, JSON
       marksman # Markdown
       nixd
+      alejandra
       python313Packages.python-lsp-server
       rust-analyzer
       clippy # Rust linter
       taplo # TOML formatter
       tinymist # Typst
+      typstyle
       typescript
       typescript-language-server
+      biome
       yaml-language-server
     ];
   };
