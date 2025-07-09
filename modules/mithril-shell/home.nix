184af09b9f3647d7e7494e75d8534777814534c5
bricked <git@bricked.dev>
2025-07-09 23:40:39 +0200

    modules/mithril-shell: add dependencies

--- modules/mithril-shell/home.nix
+++ modules/mithril-shell/home.nix
@@ -10,8 +10,11 @@
 in {
   imports = [inputs.mithril-shell.homeManagerModules.default];
 
-  config = mkIf (config.stylix.enable && cfg.integrations.stylix.enable) {
-    services.mithril-shell = {
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [grim];
+    services.swaync.enable = true;
+
+    services.mithril-shell = mkIf (config.stylix.enable && cfg.integrations.stylix.enable) {
       theme.colors.primary = config.lib.stylix.colors.base0D;
       settings.lockCommand = "${pkgs.hyprlock}/bin/hyprlock --immediate";
     };
