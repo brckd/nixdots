bcefe7740fa75c18a7677329a1ba62b03461f9e1
bricked <git@bricked.dev>
2025-07-10 14:30:14 +0200

    modules/mithril-shell: add power menu entries

--- modules/mithril-shell/home.nix
+++ modules/mithril-shell/home.nix
@@ -16,7 +16,35 @@ in {
 
     services.mithril-shell = mkIf (config.stylix.enable && cfg.integrations.stylix.enable) {
       theme.colors.primary = config.lib.stylix.colors.base0D;
-      settings.lockCommand = "${pkgs.hyprlock}/bin/hyprlock --immediate";
+      settings = {
+        lockCommand = "${pkgs.hyprlock}/bin/hyprlock --immediate";
+        powerMenuEntries = [
+          {
+            label = "Suspend";
+            command = "systemctl suspend";
+          }
+          {
+            label = "Restart";
+            command = "systemctl reboot";
+            confirmation = "Are you sure you want to restart the computer?";
+          }
+          {
+            label = "Restart to UEFI";
+            command = "systemctl reboot --firmware-setup";
+            confirmation = "Are you sure you want to restart to UEFI?";
+          }
+          {
+            label = "Power Off";
+            command = "systemctl poweroff";
+            confirmation = "Are you sure you want to power off the computer?";
+          }
+          {
+            label = "Log Out";
+            command = "uwsm stop";
+            confirmation = "Are you sure you want to log out of this session?";
+          }
+        ];
+      };
     };
   };
 }
