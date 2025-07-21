96f9c23f1811b69c9ccde0d88afcf8e1f5b9e093
bricked <git@bricked.dev>
2025-07-21 23:58:29 +0200

    modules/mithril-shell: add more dependencies

--- modules/mithril-shell/home.nix
+++ modules/mithril-shell/home.nix
@@ -5,46 +5,56 @@
   inputs,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkDefault mkMerge;
   cfg = config.services.mithril-shell;
 in {
   imports = [inputs.mithril-shell.homeManagerModules.default];
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [grim];
-    services.swaync.enable = true;
-
-    services.mithril-shell = mkIf (config.stylix.enable && cfg.integrations.stylix.enable) {
-      theme.colors.primary = config.lib.stylix.colors.base0D;
-      settings = {
-        lockCommand = "${pkgs.hyprlock}/bin/hyprlock --immediate";
-        powerMenuEntries = [
-          {
-            label = "Suspend";
-            command = "systemctl suspend";
-          }
-          {
-            label = "Restart";
-            command = "systemctl reboot";
-            confirmation = "Are you sure you want to restart the computer?";
-          }
-          {
-            label = "Restart to UEFI";
-            command = "systemctl reboot --firmware-setup";
-            confirmation = "Are you sure you want to restart to UEFI?";
-          }
-          {
-            label = "Power Off";
-            command = "systemctl poweroff";
-            confirmation = "Are you sure you want to power off the computer?";
-          }
-          {
-            label = "Log Out";
-            command = "uwsm stop";
-            confirmation = "Are you sure you want to log out of this session?";
-          }
-        ];
-      };
+    wayland.windowManager.hyprland.enable = true; # Wayland compositor
+    services.swaync.enable = true; # Notification daemon
+    services.gnome-keyring.enable = mkDefault true; # Secrets manager
+    services.gpg-agent = {
+      enable = true;
+      pinentry.package = pkgs.pinentry-gnome3; # Passphrase input
     };
+    services.mithril-shell = mkMerge [
+      {
+        integrations.hyprland.enable = true;
+      }
+
+      (mkIf (config.stylix.enable && cfg.integrations.stylix.enable) {
+        theme.colors.primary = config.lib.stylix.colors.base0D;
+        settings = {
+          lockCommand = "${pkgs.hyprlock}/bin/hyprlock --immediate";
+          powerMenuEntries = [
+            {
+              label = "Suspend";
+              command = "systemctl suspend";
+            }
+            {
+              label = "Restart";
+              command = "systemctl reboot";
+              confirmation = "Are you sure you want to restart the computer?";
+            }
+            {
+              label = "Restart to UEFI";
+              command = "systemctl reboot --firmware-setup";
+              confirmation = "Are you sure you want to restart to UEFI?";
+            }
+            {
+              label = "Power Off";
+              command = "systemctl poweroff";
+              confirmation = "Are you sure you want to power off the computer?";
+            }
+            {
+              label = "Log Out";
+              command = "uwsm stop";
+              confirmation = "Are you sure you want to log out of this session?";
+            }
+          ];
+        };
+      })
+    ];
   };
 }
