739ffe1ad67615eaf8a9962f3f016f6ad473b4a1
bricked <spider@bricked.dev>
2025-11-12 21:18:54 +0000

    modules/mithril-shell: remove unused apps

--- modules/mithril-shell/nixos.nix
+++ modules/mithril-shell/nixos.nix
@@ -27,18 +27,8 @@ in {
     services.upower.enable = mkDefault true; # Battery status
 
     environment.systemPackages = [
-      pkgs.baobab # Disk usage analyzer
-      pkgs.bottles # Wine manager
-      pkgs.cartero # API client
-      pkgs.cartridges # Game library
-      pkgs.collision # Hash viewer
-      pkgs.fragments # BitTorrent client
       pkgs.decibels # Audio player
-      pkgs.dconf-editor # DConf editor
-      pkgs.d-spy # D-Bus viewer
       pkgs.eloquent # Proofreading software
-      pkgs.fractal # Matrix client
-      pkgs.geopard # Gemini browser
       pkgs.ghostty # Terminal emulator
       pkgs.gimp3 # Image editor
       pkgs.gnome-text-editor # Text editor
@@ -46,26 +36,21 @@ in {
       pkgs.gnome-calendar # Calendar application
       pkgs.gnome-characters # Characters viewer
       pkgs.gnome-clocks # Clock application
-      pkgs.gnome-contacts # Address book
       pkgs.gnome-font-viewer # Font library
       pkgs.gnome-logs # Systemd journal viewer
       pkgs.gnome-maps # Map application
       pkgs.gnome-weather # Weather application
       pkgs.ghex # Hex editor
-      pkgs.gradia # Screenshot annotation program
       pkgs.grim # Screenshot application
-      pkgs.icon-library # Icon library
       pkgs.inspector # System information viewer
       pkgs.libreoffice # Office application
       pkgs.loupe # Image viewer
       pkgs.mission-center # System monitor
       pkgs.nautilus # File manager
       pkgs.papers # Document viewer
-      pkgs.pinta # Drawing program
       pkgs.snapshot # Camera application
       pkgs.totem # Movie player
       pkgs.tuba # Mastodon client
-      pkgs.typewriter # Typst editor
       pkgs.wl-clipboard # Clipboard manager
       (pkgs.writeShellScriptBin "xdg-terminal-exec" "ghostty -e $@")
     ];
